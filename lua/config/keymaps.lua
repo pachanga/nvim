edd6e9c66f1a37146386e9c74eb553c9347f9a9e
Pavel Shevaev <pacha.shevaev@gmail.com>
2025-06-12 11:29:51 +0300

    Experimenting with mappings and mini.move

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,14 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
---let's remove these potentially conflicting with tmux mappings
-vim.keymap.del("n", "<m-k>")
-vim.keymap.del("n", "<m-j>")
-vim.keymap.del("v", "<m-k>")
-vim.keymap.del("v", "<m-j>")
-vim.keymap.del("i", "<m-k>")
-vim.keymap.del("i", "<m-j>")
-
 vim.keymap.set("n", "<leader>p", LazyVim.pick("files"), { desc = "Find Files (Root Dir)" })
 vim.keymap.set("n", "<leader>t", "<Esc>:FzfLua tags<CR>", { desc = "Find Tags" })
 
@@ -33,6 +25,14 @@ vim.keymap.set("n", "<F3>", function()
         },
       },})
 end, { desc = "Show buffers" })
+
+--let's remove these potentially conflicting with tmux mappings
+--vim.keymap.del("n", "<m-k>")
+--vim.keymap.del("n", "<m-j>")
+--vim.keymap.del("v", "<m-k>")
+--vim.keymap.del("v", "<m-j>")
+--vim.keymap.del("i", "<m-k>")
+--vim.keymap.del("i", "<m-j>")
 
 -- Save on escape
 --vim.keymap.set("n", "<Esc><Esc>", "<Esc>:up<CR>", { noremap = true })
--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -46,6 +46,31 @@ return {
   },
 
   {
+    "echasnovski/mini.surround",
+     event = "VeryLazy",
+  },
+
+  {
+    "echasnovski/mini.move",
+    version = "*",
+    opts = {
+      mappings = {
+        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+        left = "<A-h>",
+        right = "<A-l>",
+        down = "<A-j>",
+        up = "<A-k>",
+
+        -- Move current line in Normal mode
+        line_left = "<A-h>",
+        line_right = "<A-l>",
+        line_down = "<A-j>",
+        line_up = "<A-k>",
+      },
+    },
+  },
+
+  {
     "gbprod/yanky.nvim",
     enabled = false,  
     keys = {
