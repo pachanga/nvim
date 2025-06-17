afc528eb0cb277873a7f437abff0884a86473bff
Pavel Shevaev <pacha.shevaev@gmail.com>
2025-06-17 16:02:53 +0300

    Bumping deps, adding winbar

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -140,6 +140,24 @@ return {
     event = "VeryLazy",
     opts = function(_, opts)
 
+      opts.winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {'filename'}
+      }
+
+      opts.inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {'filename'}
+      }
+
       opts.sections.lualine_c[4] = { "filename", path = 3 }
 
     end,
