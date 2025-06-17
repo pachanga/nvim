2cb67bd514418cc0137574a1afc8252c00df10f6
Pavel Shevaev <pacha.shevaev@gmail.com>
2025-06-17 08:26:56 +0300

    Tweaking snipe

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -196,7 +196,9 @@ return {
     keys = {
       {"<F3>", function () require("snipe").open_buffer_menu() end, desc = "Open Snipe buffer menu"}
     },
-    opts = {}
+    opts = {
+      ui = { position = 'center', }
+    }
   },
 
   {
