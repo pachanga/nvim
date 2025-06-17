356c50677cc28dd57d8d4ee9a2c63e9c82d56d19
Pavel Shevaev <pacha.shevaev@gmail.com>
2025-06-17 18:41:11 +0300

    Trying to configure codeium

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -171,7 +171,7 @@ return {
 
   {
     "hrsh7th/nvim-cmp",
-    enabled = true
+    enabled = false
   },
 
   {
@@ -233,32 +233,26 @@ return {
 
   {
     'saghen/blink.cmp',
-     dependencies = {
-       'Exafunction/codeium.nvim',
-     },
      opts = {
-      keymap = {
-        --preset = "enter",
-        --["<C-y>"] = { "select_and_accept" },
-        --preset = 'super-tab',
-        preset = 'default',
-        ["<Tab>"] = { "select_next", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-      },
-      completion = {
-        list = {
-          selection = {
-            preselect = false,
-            auto_insert = true,
-          }
-        }
-      },
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer', 'codeium' },
-        providers = {
-          codeium = { name = 'Codeium', module = 'codeium.blink', async = true },
-        },
-      }
+       keymap = {
+         --preset = "enter",
+         --["<C-y>"] = { "select_and_accept" },
+         --preset = 'super-tab',
+         preset = 'default',
+         ["<Tab>"] = { "select_next", "fallback" },
+         ["<S-Tab>"] = { "select_prev", "fallback" },
+       },
+       completion = {
+         list = {
+           selection = {
+             preselect = false,
+             auto_insert = true,
+           }
+         }
+       },
+       --sources = {
+       --  default = { 'lsp', 'path', 'snippets', 'buffer'},
+       --}
     },
     cmdline = {
       enabled = false,
@@ -500,8 +494,52 @@ return {
     },
     config = function()
         require("codeium").setup({
+           enable_cmp_source = false,
+           virtual_text = {
+              enabled = true,
+
+              key_bindings = {
+                --accept = false -- handled by nvim-cmp / blink.cmp
+                accept = "<F1>",
+                next = "<M-]>",
+                prev = "<M-[>",
+              },
+
+              ---- These are the defaults
+              ---- Set to true if you never want completions to be shown automatically.
+              --manual = false,
+              ---- A mapping of filetype to true or false, to enable virtual text.
+              --filetypes = {},
+              ---- Whether to enable virtual text of not for filetypes not specifically listed above.
+              --default_filetype_enabled = true,
+              ---- How long to wait (in ms) before requesting completions after typing stops.
+              --idle_delay = 75,
+              ---- Priority of the virtual text. This usually ensures that the completions appear on top of
+              ---- other plugins that also add virtual text, such as LSP inlay hints, but can be modified if
+              ---- desired.
+              --virtual_text_priority = 65535,
+              ---- Set to false to disable all key bindings for managing completions.
+              --map_keys = true,
+              ---- The key to press when hitting the accept keybinding but no completion is showing.
+              ---- Defaults to \t normally or <c-n> when a popup is showing. 
+              --accept_fallback = nil,
+              ---- Key bindings for managing completions in virtual text mode.
+              --key_bindings = {
+              --    -- Accept the current completion.
+              --    accept = "<Tab>",
+              --    -- Accept the next word.
+              --    accept_word = false,
+              --    -- Accept the next line.
+              --    accept_line = false,
+              --    -- Clear the virtual text.
+              --    clear = false,
+              --    -- Cycle to the next completion.
+              --    next = "<M-]>",
+              --    -- Cycle to the previous completion.
+              --    prev = "<M-[>",
+              --}
+          }
         })
     end
   },
-
 }
