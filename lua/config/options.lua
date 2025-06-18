c278c2fdfc471360e666cc2aebd39298ad2c4826
Pavel Shevaev <pacha.shevaev@gmail.com>
2025-06-18 17:06:37 +0300

    Adding BHL LSP config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,7 +7,7 @@ vim.g.maplocalleader = "\\"
 vim.g.snacks_animate = false
 vim.opt.relativenumber = false
 vim.g.root_spec = { ".git" }
-vim.g.autoformat = true
+vim.g.autoformat = false
 vim.opt.wrap = true
 
 if vim.fn.executable("rg") then
@@ -16,9 +16,22 @@ if vim.fn.executable("rg") then
   vim.opt.grepformat = "%f:%l:%c:%m,%f:%l:%m"
 end
 
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = "*.bhl",
-  command = "setfiletype go",
-})
+--vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+--  pattern = "*.bhl",
+--  command = "setfiletype go",
+--})
 
 --vim.g.lazyvim_php_lsp = "intelephense"
+
+vim.lsp.enable("bhl")
+vim.lsp.config("bhl", {
+  cmd = { os.getenv("HOME") .. "/dev/bhl/bhl", "lsp", "--log-file=" .. os.getenv("HOME") .. "/tmp/bhl.lsp" },
+  filetypes = { "bhl" },
+  root_markers = { "bhl.proj" },
+  settings = {
+    --bhl = {
+    --  log_level = "debug",
+    --},
+  },
+})
+vim.cmd([[ au BufNewFile,BufRead /*.bhl setf bhl ]])
