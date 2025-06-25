97534645bca23a8b7353cb036ad0ef11f334e8bf
Pavel Shevaev <pacha.shevaev@gmail.com>
2025-06-25 14:04:45 +0300

    Migrating to another multicursors plugin

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -280,9 +280,27 @@ return {
     }
   },
 
+  --{
+  --  'mg979/vim-visual-multi',
+  --  branch = "master"
+  --},
+
   {
-    'mg979/vim-visual-multi',
-    branch = "master"
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        'nvimtools/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+            {
+                mode = { 'v', 'n' },
+                '<C-n>',
+                '<cmd>MCstart<cr>',
+                desc = 'Create a selection for selected text or word under the cursor',
+            },
+        },
   },
 
 
