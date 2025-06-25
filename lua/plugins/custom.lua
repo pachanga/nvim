16b27d5ed437efa67ecee621550b87da3ed53600
Pavel Shevaev <pacha.shevaev@gmail.com>
2025-06-25 13:53:32 +0300

    Improving incline visuals

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -190,7 +190,20 @@ return {
   {
     'b0o/incline.nvim',
     config = function()
-      require('incline').setup()
+      require('incline').setup{
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+          local modified = vim.bo[props.buf].modified
+          return {
+            ' ',
+            { filename, gui = 'bold' },
+            modified and { ' *', guifg = '#888888', gui = 'bold' } or '',
+            ' ',
+            guibg = '#44406e',
+            guifg = '#eeeeee',
+          }
+        end
+      }
     end,
     -- Optional: Lazy load Incline
     event = 'VeryLazy',
